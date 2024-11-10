d7b7ad28b9ef98e152da48db7e2c1bb9722a0bad
Eduardo da Silveira <esilveira@inf.ufsm.br>
2024-11-10 16:59:58 -0300

    Implement functions to create vectors from dynamic and static arrays.

--- src/utils.h
+++ src/utils.h
@@ -6,8 +6,13 @@
 
 gsl_vector **vectors_alloc(size_t *shape, size_t n);
 void vectors_free(gsl_vector **vs, size_t n);
-gsl_matrix **matrices_alloc(size_t *shape, size_t n);
-void matrices_free(gsl_matrix **ms, size_t n);
+gsl_matrix **weights_alloc(size_t *shape, size_t n);
+void weights_free(gsl_matrix **ms, size_t n);
+
+gsl_vector *vector_alloc_from(double *x, size_t n);
+gsl_vector *vector_alloc_from_static(size_t n, double x[]);
+gsl_matrix *weights_alloc_from(double **x, size_t m, size_t n);
+gsl_matrix *weights_alloc_from_static(size_t m, size_t n, double [][n]);
 
 void mvmul(gsl_matrix *w, gsl_vector *a, gsl_vector *b, gsl_vector *z);
 void vfapply(gsl_vector *dst, gsl_vector *src, double (*f)(double x));
